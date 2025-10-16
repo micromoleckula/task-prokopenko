813cfa8b2004fc72d78e245e0ef98ab054f95001
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-10-16 12:54:55 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./App.module.css";
-import { Header } from "./components/header/Header.jsx";
+import { Header } from "./components/Header/Header.jsx";
 import { Located } from "./components/Located/Located.jsx";
 import { TotalAmount } from "./components/TotalAmount/TotalAmount.jsx";
 
